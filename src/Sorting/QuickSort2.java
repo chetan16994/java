edab9fa4d4ceabb003aa4e4ce200b007d3443f4e
chetan16994 <chetan16994@gmail.com>
2021-03-01 14:12:36 -0700

    .

---
+++ src/Sorting/QuickSort2.java
@@ -0,0 +1,46 @@
+package Sorting;
+
+public class QuickSort2 {
+
+//    using end as pivot
+    public void quickSort(int arr[], int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(arr, begin, end);
+            quickSort(arr, begin, partitionIndex-1);
+            quickSort(arr, partitionIndex+1, end);
+        }
+    }
+    private int partition(int arr[], int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin-1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                int swapTemp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = swapTemp;
+            }
+        }
+
+        int swapTemp = arr[i+1];
+        arr[i+1] = arr[end];
+        arr[end] = swapTemp;
+
+        return i+1;
+    }
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int [] arr={38,1,88,64,25,12,22,11,24};
+//        int [] arr={1,2,3,4,5,6};
+        QuickSort2 obj=new QuickSort2();
+        obj.quickSort(arr,0,arr.length-1);
+        printArray(arr);
+    }
+}
